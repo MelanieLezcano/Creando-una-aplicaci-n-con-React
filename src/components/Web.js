17537fd6133588785d9e119d96cc0abec48a5231
MelanieLezcano <melanie.s.lezcano@gmail.com>
2023-01-02 20:01:40 -0300

    encabezado

---
+++ src/components/Web.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Encabezado from './Encabezado';
+function Web(){
+  return (
+    
+    <div className="container">
+      <Encabezado />
+     <h1>Bienvenido a nuestro primer proyecto en React</h1>
+    </div>
+  )
+
+}
+export default Web;
